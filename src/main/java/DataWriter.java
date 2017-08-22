ce3c323c7a7ec6658d08109015792a4fce68043b
mengfeifei <happymff@126.com>
2017-08-22 21:49:02 +0800

    update with gitignore

---
+++ src/main/java/DataWriter.java
@@ -0,0 +1,29 @@
+/**
+ * Created by mengfeifei on 2017/8/22.
+ */
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.skyscreamer.jsonassert.JSONCompareResult;
+
+
+public class DataWriter {
+
+    public DataWriter() {
+    }
+
+
+    public static void writeData(XSSFSheet comparsionSheet, JSONCompareResult result, String id, String test_case) {
+    }
+
+    public static void writeData(XSSFSheet resultSheet, String aFalse, String id, String test_case, int i) {
+    }
+
+    public static void writeData(XSSFSheet outputSheet, String s, String ID, String test_case) {
+    }
+
+    public static void writeData(XSSFSheet resultSheet, double totalcase, double failedcase, String startTime, String endTime) {
+    }
+
+    public static void writeData(XSSFSheet comparsionSheet, String baseline_message, String s, String id, String test_case) {
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+/**
+ * Created by mengfeifei on 2017/8/22.
+ */
+public class Utils {
+
+    public static Map<String,RecordHandler> sortmap(Map<String, RecordHandler> myInput) {
+        Map<String,RecordHandler> map=null;
+        return map;
+    }
+}
